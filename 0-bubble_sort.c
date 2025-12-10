5a7b0dd3bcad7a8d9bcdd9849b8e4140c7265624
laetitiagrondin <laetitiagrondin>
2025-12-10 11:32:27 +0000

    Add file 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * bubble_sort - Sorts an array of integers in ascendin gorder
+ * using the Bubble sort algorithm
+ * @array: pointer to the array of integers to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size);
+{
+	size_t i, j, k;
+	int temp;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				for (k = 0; k < size; k++)
+				{
+					if (k > 0)
+					{
+						printf(", ");
+					}
+					printf("%d", array[k]);
+				}
+				print("\n");
+			}
+		}
+	}
+}
